461714f2f8282aed76a964722ebe1f221ed07a02
Xing-Boy <1102121749@qq.com>
2020-02-14 22:23:02 +0800

    Revert "6大类基本矿建已建立完成"
    This reverts commit b3c9d8d7f402558488f13d0e3e4fe56304d7daed.

--- HFM/Components/Nuclide.cs
+++ HFM/Components/Nuclide.cs
@@ -2,10 +2,10 @@
  * ________________________________________________________________________________ 
  *
  *  描述：
- *  作者：邢家宁
+ *  作者：
  *  版本：
- *  创建时间：2020/2/14
- *  类名：核素类 Nuclide
+ *  创建时间：
+ *  类名：
  *  
  *  Copyright (C) 2020 TIT All rights reserved.
  *_________________________________________________________________________________
@@ -20,117 +20,5 @@ namespace HFM.Components
 {
     class Nuclide
     {
-        #region 属性
-        private int _alphaNuclide;//α核素可用
-        private int _betaNuclide;//β核素可用
-        private int _clothesNuclide;//C（衣物探头）核素可用
-        private string _alphaNuclideUser;//α核素选择
-        private string _betaNuclideUser;//β核素选择
-        private string _clothesNucliderUser;//C（衣物探头）核素选择
-        
-
-        public int AlphaNuclide { get => _alphaNuclide; set => _alphaNuclide = value; }
-        public int BetaNuclide { get => _betaNuclide; set => _betaNuclide = value; }
-        public int ClothesNuclide { get => _clothesNuclide; set => _clothesNuclide = value; }
-        public string AlphaNuclideUser { get => _alphaNuclideUser; set => _alphaNuclideUser = value; }
-        public string BetaNuclideUser { get => _betaNuclideUser; set => _betaNuclideUser = value; }
-        public string ClothesNucliderUser { get => _clothesNucliderUser; set => _clothesNucliderUser = value; }
-        #endregion
-        #region 构造函数
-        public Nuclide()
-        { }
-        /// <summary>
-        /// 参数构造
-        /// </summary>
-        /// <param name="_alphaNuclide"></param>
-        /// <param name="_betaNuclide"></param>
-        /// <param name="_clothesNuclide"></param>
-        /// <param name="_alphaNuclideUser"></param>
-        /// <param name="_betaNuclideUser"></param>
-        /// <param name="_clothesNucliderUser"></param>
-        public Nuclide(int _alphaNuclide, int _betaNuclide, int _clothesNuclide, string _alphaNuclideUser, string _betaNuclideUser, string _clothesNucliderUser)
-        {
-            this._alphaNuclide = _alphaNuclide;
-            this._betaNuclide = _betaNuclide;
-            this._clothesNuclide = _clothesNuclide;
-            this._alphaNuclideUser = _alphaNuclideUser;
-            this._betaNuclideUser = _betaNuclideUser;
-            this._clothesNucliderUser = _clothesNucliderUser;
-        }
-
-        #endregion
-        #region 方法
-        /// <summary>
-        /// 从数据库中查询当前核素信息
-        /// </summary>
-        /// <returns></returns>
-        public Nuclide GetNuclide()
-        {
-            return null;
-        }
-        /// <summary>
-        /// 获得当前α核素选择
-        /// </summary>
-        /// <returns></returns>
-        public string GetAlphaNuclideUser()
-        {
-            return null;
-        }
-        /// <summary>
-        /// 获得当前β核素选择
-        /// </summary>
-        /// <returns></returns>
-        public string GetBetaNuclideUser()
-        {
-            return null;
-        }
-        /// <summary>
-        /// 获得当前C核素选择
-        /// </summary>
-        /// <returns></returns>
-        public string GetClothesNuclideUser()
-        {
-            return null;
-        }
-        /// <summary>
-        /// 更新数据库中的核素信息
-        /// </summary>
-        /// <param name="nuclide"></param>
-        /// <returns></returns>
-        public bool SetNuclide(Nuclide nuclide)
-        {
-            return true;
-        }
-        /// <summary>
-        /// 设置当前α核素选择，将数据库HFM_MainPreference中的
-        /// AlphaNuclideUser字段按照参数alphaNuclideUser进行设置
-        /// </summary>
-        /// <param name="alphaNuclideUser"></param>
-        /// <returns></returns>
-        public bool SetAlphaNuclideUser(int alphaNuclideUser)
-        {
-            return true;
-        }
-        /// <summary>
-        /// 设置当前β核素选择，将数据库HFM_MainPreference中的
-        /// BetaNuclideUser字段按照参数betaNuclideUser 进行设置
-        /// </summary>
-        /// <param name="betaNuclideUser"></param>
-        /// <returns></returns>
-        public bool SetBetaNuclideUser(int betaNuclideUser)
-        {
-            return true;
-        }
-        /// <summary>
-        /// //设置当前C核素选择，将数据库HFM_MainPreference中的
-        /// ClothesNuclideUser字段按照参数clothesNuclideUser进行设置
-        /// </summary>
-        /// <param name="clothesNuclideUser"></param>
-        /// <returns></returns>
-        public bool SetClothesNuclideUser(int clothesNuclideUser)
-        {
-            return true;
-        }
-        #endregion
     }
 }
--- HFM/Components/SystemParameter.cs
+++ HFM/Components/SystemParameter.cs
@@ -2,10 +2,10 @@
  * ________________________________________________________________________________ 
  *
  *  描述：
- *  作者：邢家宁
+ *  作者：
  *  版本：
  *  创建时间：
- *  类名：系统参数类  SystemParameter
+ *  类名：
  *  
  *  Copyright (C) 2020 TIT All rights reserved.
  *_________________________________________________________________________________
@@ -20,73 +20,5 @@ namespace HFM.Components
 {
     class SystemParameter
     {
-        #region 属性
-        private string _measurementUnit;//测量单位
-        private int _selfCheckTime;//自检时间
-        private int _smoothingTime;//平滑时间
-        private int _measuringTime;//测量时间
-        private int _alarmTime;//报警时间
-        private int _bkgUpdate;//强制本底次数
-        private int _clothOfflineTime;//衣物离线自检时间
-        private bool _isEnglish;//当前是否英文版本
-
-        
-
-        public string MeasurementUnit { get => _measurementUnit; set => _measurementUnit = value; }
-        public int SelfCheckTime { get => _selfCheckTime; set => _selfCheckTime = value; }
-        public int SmoothingTime { get => _smoothingTime; set => _smoothingTime = value; }
-        public int MeasuringTime { get => _measuringTime; set => _measuringTime = value; }
-        public int AlarmTime { get => _alarmTime; set => _alarmTime = value; }
-        public int BkgUpdate { get => _bkgUpdate; set => _bkgUpdate = value; }
-        public int ClothOfflineTime { get => _clothOfflineTime; set => _clothOfflineTime = value; }
-        public bool IsEnglish { get => _isEnglish; set => _isEnglish = value; }
-
-        #endregion
-        #region 构造函数
-        public SystemParameter()
-        { }
-        /// <summary>
-        /// 参数构造
-        /// </summary>
-        /// <param name="_measurementUnit"></param>
-        /// <param name="_selfCheckTime"></param>
-        /// <param name="_smoothingTime"></param>
-        /// <param name="_measuringTime"></param>
-        /// <param name="_alarmTime"></param>
-        /// <param name="_bkgUpdate"></param>
-        /// <param name="_clothOfflineTime"></param>
-        /// <param name="_isEnglish"></param>
-        public SystemParameter(string _measurementUnit, int _selfCheckTime, int _smoothingTime, int _measuringTime, int _alarmTime, int _bkgUpdate, int _clothOfflineTime, bool _isEnglish)
-        {
-            this._measurementUnit = _measurementUnit;
-            this._selfCheckTime = _selfCheckTime;
-            this._smoothingTime = _smoothingTime;
-            this._measuringTime = _measuringTime;
-            this._alarmTime = _alarmTime;
-            this._bkgUpdate = _bkgUpdate;
-            this._clothOfflineTime = _clothOfflineTime;
-            this._isEnglish = _isEnglish;
-        }
-        #endregion
-        #region 方法
-        /// <summary>
-        /// 从数据库中查询当前工厂参数并返回工厂参数对象
-        /// </summary>
-        /// <returns></returns>
-        public SystemParameter GetParameter()
-        {
-            return null;
-        }
-        /// <summary>
-        /// 更新数据库中工厂设置
-        /// </summary>
-        /// <param name="systemParameter"></param>
-        /// <returns></returns>
-        public bool SetParameter(SystemParameter systemParameter)
-        {
-            return true;
-        }
-
-        #endregion
     }
 }
