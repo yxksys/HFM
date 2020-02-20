8ce170b0078f7dbe958774b65bfd0275b9973bde
yangxukai <118179404@qq.com>
2020-02-20 09:39:58 +0800

    初始化硬件检测窗体

---
+++ HFM/FrmTestHardware.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace HFM
+{
+    partial class FrmTestHardware
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmTestHardware
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 516);
+            this.ControlBox = false;
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmTestHardware";
+            this.Text = "硬件检测";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
