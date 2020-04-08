97b30cc8d71e19356b38ae75b9d28e458c716b71
yangxukai <118179404@qq.com>
2020-04-09 05:42:07 +0800

    修改密码窗体翻译完成

--- HFM/FrmModifyPasssword.Designer.cs
+++ HFM/FrmModifyPasssword.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmModifyPasssword));
             this.LblOldPassword = new System.Windows.Forms.Label();
             this.TxtOldPassword = new System.Windows.Forms.TextBox();
             this.LblNewPassword = new System.Windows.Forms.Label();
@@ -40,91 +41,55 @@
             // 
             // LblOldPassword
             // 
-            this.LblOldPassword.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblOldPassword.Location = new System.Drawing.Point(13, 46);
+            resources.ApplyResources(this.LblOldPassword, "LblOldPassword");
             this.LblOldPassword.Name = "LblOldPassword";
-            this.LblOldPassword.Size = new System.Drawing.Size(135, 18);
-            this.LblOldPassword.TabIndex = 21;
-            this.LblOldPassword.Text = "当前密码";
-            this.LblOldPassword.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // TxtOldPassword
             // 
-            this.TxtOldPassword.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.TxtOldPassword.Location = new System.Drawing.Point(154, 44);
+            resources.ApplyResources(this.TxtOldPassword, "TxtOldPassword");
             this.TxtOldPassword.Name = "TxtOldPassword";
-            this.TxtOldPassword.PasswordChar = '*';
-            this.TxtOldPassword.Size = new System.Drawing.Size(132, 26);
-            this.TxtOldPassword.TabIndex = 29;
             this.TxtOldPassword.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TxtOldPassword_MouseClick);
             // 
             // LblNewPassword
             // 
-            this.LblNewPassword.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblNewPassword.Location = new System.Drawing.Point(32, 77);
+            resources.ApplyResources(this.LblNewPassword, "LblNewPassword");
             this.LblNewPassword.Name = "LblNewPassword";
-            this.LblNewPassword.Size = new System.Drawing.Size(116, 18);
-            this.LblNewPassword.TabIndex = 22;
-            this.LblNewPassword.Text = "新的密码";
-            this.LblNewPassword.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // TxtNewPassword
             // 
-            this.TxtNewPassword.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.TxtNewPassword.Location = new System.Drawing.Point(154, 76);
+            resources.ApplyResources(this.TxtNewPassword, "TxtNewPassword");
             this.TxtNewPassword.Name = "TxtNewPassword";
-            this.TxtNewPassword.PasswordChar = '*';
-            this.TxtNewPassword.Size = new System.Drawing.Size(132, 26);
-            this.TxtNewPassword.TabIndex = 30;
             this.TxtNewPassword.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TxtNewPassword_MouseClick);
             // 
             // LblFinalPsaaword
             // 
-            this.LblFinalPsaaword.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblFinalPsaaword.Location = new System.Drawing.Point(24, 108);
+            resources.ApplyResources(this.LblFinalPsaaword, "LblFinalPsaaword");
             this.LblFinalPsaaword.Name = "LblFinalPsaaword";
-            this.LblFinalPsaaword.Size = new System.Drawing.Size(124, 18);
-            this.LblFinalPsaaword.TabIndex = 31;
-            this.LblFinalPsaaword.Text = "重复密码";
-            this.LblFinalPsaaword.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // TxtFinalPsaaword
             // 
-            this.TxtFinalPsaaword.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.TxtFinalPsaaword.Location = new System.Drawing.Point(154, 108);
+            resources.ApplyResources(this.TxtFinalPsaaword, "TxtFinalPsaaword");
             this.TxtFinalPsaaword.Name = "TxtFinalPsaaword";
-            this.TxtFinalPsaaword.PasswordChar = '*';
-            this.TxtFinalPsaaword.Size = new System.Drawing.Size(132, 26);
-            this.TxtFinalPsaaword.TabIndex = 32;
             this.TxtFinalPsaaword.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TxtFinalPsaaword_MouseClick);
             // 
             // BtnConfirm
             // 
-            this.BtnConfirm.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnConfirm.Location = new System.Drawing.Point(84, 154);
+            resources.ApplyResources(this.BtnConfirm, "BtnConfirm");
             this.BtnConfirm.Name = "BtnConfirm";
-            this.BtnConfirm.Size = new System.Drawing.Size(75, 32);
-            this.BtnConfirm.TabIndex = 33;
-            this.BtnConfirm.Text = "确定";
             this.BtnConfirm.UseVisualStyleBackColor = true;
             this.BtnConfirm.Click += new System.EventHandler(this.BtnConfirm_Click);
             // 
             // BtnCancel
             // 
-            this.BtnCancel.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCancel.Location = new System.Drawing.Point(193, 154);
+            resources.ApplyResources(this.BtnCancel, "BtnCancel");
             this.BtnCancel.Name = "BtnCancel";
-            this.BtnCancel.Size = new System.Drawing.Size(75, 32);
-            this.BtnCancel.TabIndex = 34;
-            this.BtnCancel.Text = "取消";
             this.BtnCancel.UseVisualStyleBackColor = true;
             this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
             // 
             // FrmModifyPasssword
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 216);
             this.Controls.Add(this.BtnCancel);
             this.Controls.Add(this.BtnConfirm);
             this.Controls.Add(this.TxtFinalPsaaword);
@@ -134,8 +99,6 @@
             this.Controls.Add(this.TxtOldPassword);
             this.Controls.Add(this.LblOldPassword);
             this.Name = "FrmModifyPasssword";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "修改密码";
             this.ResumeLayout(false);
             this.PerformLayout();
 
