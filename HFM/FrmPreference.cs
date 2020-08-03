5cee37120e3feb2fd5b881c9a61488f135575948
凰 凤 <118179404@qq.com>
2020-08-03 18:08:59 +0800

    版本3.0.6.17设置中,系统参数增加超时设置

--- HFM/FrmPreference.cs
+++ HFM/FrmPreference.cs
@@ -200,7 +200,8 @@ namespace HFM
             TxtMeasuringTime.Text = system.MeasuringTime.ToString();
             CmbMeasurementUnit.Text = system.MeasurementUnit.ToString();
             TxtAlarmTime.Text = system.AlarmTime.ToString();
-            TxtBKGUpdate.Text = system.BkgUpdate.ToString();            
+            TxtBKGUpdate.Text = system.BkgUpdate.ToString();
+            TxtTimeOut.Text = system.TimeOut.ToString();
             #endregion
 
             #region 工厂参数
@@ -1067,6 +1068,7 @@ namespace HFM
                 system.MeasurementUnit = CmbMeasurementUnit.Text;
                 system.AlarmTime = int.Parse(TxtAlarmTime.Text);
                 system.BkgUpdate = int.Parse(TxtBKGUpdate.Text);
+                system.TimeOut = int.Parse(TxtTimeOut.Text);
             }
             catch (Exception)
             {
@@ -1092,6 +1094,11 @@ namespace HFM
             if (system.AlarmTime<2)
             {
                 MessageBox.Show(@"报警时间必须大于 1s ！", @"提示", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                return;
+            }
+            if (system.TimeOut < 5)
+            {
+                MessageBox.Show(@"检测时间超时必须大于 5s ！", @"提示", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
                 return;
             }
             #endregion
@@ -2580,6 +2587,15 @@ namespace HFM
         private void TxtSmoothingFactor_Enter(object sender, EventArgs e)
         {
             FrmKeyIn.DelegatesKeyInTextBox(TxtSmoothingFactor);
+        }
+        /// <summary>
+        /// 超时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TxtTimeOut_Enter(object sender, EventArgs e)
+        {
+            FrmKeyIn.DelegatesKeyInTextBox(TxtTimeOut);
         }
         #endregion
 
